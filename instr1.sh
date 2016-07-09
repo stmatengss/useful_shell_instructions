73b64b569f2a0f8506dd366c4e318c212479bc4d
stmatengss <805522925@qq.com>
2016-07-09 16:38:57 +0800

    Create instr1.sh

---
+++ instr1.sh
@@ -0,0 +1,8 @@
+uniq doudi_cb.csv > doudi_cb_uniq.csv
+cat a > b
+wc -l
+sort doudi_cb.csv|uniq  > doudi_cb_uniq.csv
+grep -vwf file1 file2
+PATH = $PATH:$HOME/bin
+sed -n '10,42p' file_name
+filesize=`wc -c < filename`
