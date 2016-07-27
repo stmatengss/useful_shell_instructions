192334fddbb1fdfe08b4e3e36a06e7431509977b
stmatengss <805522925@qq.com>
2016-07-27 14:01:04 +0800

    Update instr1.sh

--- instr1.sh
+++ instr1.sh
@@ -6,3 +6,29 @@ grep -vwf file1 file2
 PATH = $PATH:$HOME/bin
 sed -n '10,42p' file_name
 filesize=`wc -c < filename`
+
+du -sh file_name
+zip -r mydata.zip mydata
+：%s/vivian/sky/g
+gggUG #整篇文章小写转化为大写
+ggguG #整篇文章大写转化为小写
+cat file1 >> file2
+awk '{print $2}' a.txt >b.txt #从a.txt中取第一列并输出到b.txt中
+set number
+for file in `ls`
+do
+	echo $file
+done
+:%s/vivian/sky/g
+date +%Y%m%d$H%M%S
+
+cat -n 文件路径 | tail -n +5 | head -n 6 
+
+5>>
+
+grep '^yb' pdf_tables.data > pdf_tables_2.data
+diff a b
+
+if [ ! -f "$myFile" ]; then 
+touch "$myFile" 
+fi 
