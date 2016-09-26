35ccfdf3af674fdcff85200af975c6627c50a604
stmatengss <805522925@qq.com>
2016-09-26 14:20:55 +0800

    Update instr4.sh

--- instr4.sh
+++ instr4.sh
@@ -7,3 +7,6 @@ echo ""|mail -s "" someone@some
 who #lookup someone's tty id
 
 wall "" #send message to someone else.
+
+# tmp pip
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple some-package
