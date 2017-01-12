dbb86b8d2f0de668c9c0ef18f1ece99700a97f7f
Teng Ma <805522925@qq.com>
2017-01-13 00:45:29 +0800

    Update Linux_kernel.sh

--- Linux_kernel.sh
+++ Linux_kernel.sh
@@ -2,4 +2,8 @@ echo 3 > /proc/sys/vm/drop_caches # release cache memory
 
 sysctl [-n] [-e] -p <filename> (default /etc/sysctl.conf) # system kernel setting
 
-more /var/log/dmesg | grep cache
+more /var/log/dmesg | grep cache # look up cache
+
+lscpu # look up cpu
+
+fdisk -l # look up cap of disk
