c7c3707df2fcecf97d19a523ce6f38b7cf7ddaf8
Teng Ma <805522925@qq.com>
2017-02-13 21:09:04 +0800

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -9,3 +9,7 @@ gcc -DNODEBUG ... # Define a macro named NODEBUG
 gcc -Og/O1/O2/O3 ... # The different levels of optmization
 
 gcc -L/-l ... # contain the refer library when compiling
+
+gcc -Werror -Wwarning -w ... # diable or allow the print of warning
+
+gcc -g ... # for GDB debug
