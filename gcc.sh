f36c5066beb3432e596408a0a8ae3dd6c2d41916
Teng Ma <805522925@qq.com>
2017-02-13 21:12:11 +0800

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -13,3 +13,6 @@ gcc -L/-l ... # contain the refer library when compiling
 gcc -Werror -Wwarning -w ... # diable or allow the print of warning
 
 gcc -g ... # for GDB debug
+
+ar [cr] test_all.a test1.o test2.o # build up a new lib with test1.c & test2.c
+ 
