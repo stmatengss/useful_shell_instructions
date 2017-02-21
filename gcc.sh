268c932d95149cf3b593ca298a970e851c864973
Teng Ma <805522925@qq.com>
2017-02-21 11:36:13 +0800

    Update gcc.sh

--- gcc.sh
+++ gcc.sh
@@ -17,3 +17,8 @@ gcc -g ... # for GDB debug
 ar [cr] test_all.a test1.o test2.o # build up a new lib with test1.c & test2.c
 
 ldd test.so[/usr/lib/xxx.so] # find the details of 'so' type file
+
+# How to solve './tests: error while loading shared libraries: xxx.so.0:
+# cannot open shared object file: No such file or directory'
+sudo echo '/usr/local/lib' >> /etc/ld.so.conf
+sudo /sbin/ldconfig –v # add xx.so to the ldd environment
