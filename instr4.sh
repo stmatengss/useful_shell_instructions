2e0bdef3a36ddb70f1fef058088defb34176574e
Teng Ma <805522925@qq.com>
2017-03-08 19:35:33 +0800

    Update instr4.sh

--- instr4.sh
+++ instr4.sh
@@ -73,3 +73,5 @@ lsmod | grep "xxx" # lookup the working state of linux kernel modules.
 sudo apt-get -y --force-yes install # assure the alert automatically when installing the software
 
 taskset # A tool for indicating a core to a thread
+
+pgrep # find pid number through the pid name 
