4908317d37d7b00bfef24d6188310bf6bd261f37
Teng Ma <805522925@qq.com>
2017-03-15 19:27:56 +0800

    Create cal.sh

---
+++ cal.sh
@@ -0,0 +1,11 @@
+# SUM
+cat data|awk '{sum+=$1} END {print "Sum = ", sum}'
+ 
+# AVE
+cat data|awk '{sum+=$1} END {print "Average = ", sum/NR}'
+ 
+# MAX
+cat data|awk 'BEGIN {max = 0} {if ($1>max) max=$1 fi} END {print "Max=", max}'
+ 
+# MIN
+awk 'BEGIN {min = 1999999} {if ($1<min) min=$1 fi} END {print "Min=", min}'
