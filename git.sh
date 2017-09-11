75778245b37d9a73ae03692fb1be70942991c7fd
Teng Ma <805522925@qq.com>
2017-09-11 16:51:17 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -5,3 +5,5 @@ git branch -r # show all branchs
 git branch # show u r in which branch now
 
 git revert HEAD # cancel the previous commit
+
+git reset --hard commit_id  # back up to a history commit
