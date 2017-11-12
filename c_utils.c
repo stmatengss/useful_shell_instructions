1696fcea8d9d253215d763866e8acb61a0676caf
Teng Ma <805522925@qq.com>
2017-11-12 15:46:04 +0800

    Update c_utils.c

--- c_utils.c
+++ c_utils.c
@@ -29,3 +29,20 @@ struct xxx{
 	...
 	...
 } __attribute__((aligned(CACHE_LINE_SIZE)))
+
+
+#ifndef LOG_LEVEL
+#ifdef DEBUG
+#define LOG_LEVEL 10
+#else // !DEBUG
+#define LOG_LEVEL 1
+#endif // DEBUG
+#endif // LOG_LEVEL
+
+#define LOG(LEVEL, STR) do {\
+    if ((LEVEL) < LOG_LEVEL) {\
+        printf("%-20s | %3d |  ",__FUNCTION__,__LINE__); \
+        printf STR; \
+        fflush(stdout); \
+    } \
+} while(0);
