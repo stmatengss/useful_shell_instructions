b8bdc2f6daebf07c665b87422868ec88552cd798
Teng Ma <805522925@qq.com>
2017-12-29 01:39:24 -0600

    Update cal.sh

--- cal.sh
+++ cal.sh
@@ -9,3 +9,6 @@ cat data|awk 'BEGIN {max = 0} {if ($1>max) max=$1 fi} END {print "Max=", max}'
  
 # MIN
 awk 'BEGIN {min = 1999999} {if ($1<min) min=$1 fi} END {print "Min=", min}'
+
+# WORDCOUNT
+sort $1 | uniq -c | sort -n -r 
