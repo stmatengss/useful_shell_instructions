7cb9586810c95433926ad8b316b9cdefda35ea1d
Teng Ma <805522925@qq.com>
2018-07-30 19:41:43 +0800

    Update perf.sh

--- perf.sh
+++ perf.sh
@@ -1,1 +1,4 @@
 gprof ./bin/test_skip_list | gprof2dot | dot -Tpng -o report.png
+
+perf stat -e task-clock -i -I 200 -D 4000 -o back-end.cpu.6.data ./bin/test_queue -b  
+perf stat -e task-clock -I 1000 -o front-end.cpu.6.data bin/test_bst -f -s teaker-10
