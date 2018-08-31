7393a98dbc4993924fea52be98b4d378a38eca9b
Teng Ma <805522925@qq.com>
2018-08-31 10:04:15 +0800

    Update instr4.sh

--- instr4.sh
+++ instr4.sh
@@ -160,3 +160,6 @@ sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 60 --slave 
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.7 40 --slave /usr/bin/g++ g++ /usr/bin/g++-4.7 
 sudo update-alternatives --config gcc
 # Solve the the conflict of gcc
+
+fusermount -u . 
+# Cancel mount
