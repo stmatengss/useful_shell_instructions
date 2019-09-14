46a6dbf9a844396012d27600fcc9501707701d47
Teng Ma <805522925@qq.com>
2019-09-14 14:32:11 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -2,3 +2,6 @@ sudo docker commit -m "init" -a="teng" cranky_keller teng/centos
 sudo docker ps -l
 docker create --net=host --device=/dev/infiniband/uverbs0 --device=/dev/infiniband/rdma_cm -t -i centos /bin/bash
 docker images
+sudo docker tag stmatengss/centos-rdma stmatengss/centos-rdma
+sudo docker push stmatengss/centos-rdma
+docker login
