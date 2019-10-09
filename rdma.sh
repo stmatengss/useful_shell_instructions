29b68d8748114db340f29c895a2841589a4eb2cc
Teng Ma <805522925@qq.com>
2019-10-09 14:48:10 +0800

    Update rdma.sh

--- rdma.sh
+++ rdma.sh
@@ -54,3 +54,18 @@ ib_sa
 ib_mad   
 ib_core  
 ib_addr  
+
+# RDMA stack modules
+sudo modprobe rdma_cm
+sudo modprobe ib_uverbs
+sudo modprobe rdma_ucm
+sudo modprobe ib_ucm
+sudo modprobe ib_umad
+sudo modprobe ib_ipoib
+# RDMA devices low-level drivers
+sudo modprobe mlx4_ib
+sudo modprobe mlx4_en
+sudo modprobe iw_cxgb3
+sudo modprobe iw_cxgb4
+sudo modprobe iw_nes
+sudo modprobe iw_c2
