446143d522a4d8daeb33b6c60adfb47359be5f66
Teng Ma <805522925@qq.com>
2020-09-26 22:29:56 -0500

    Update rdma_usual.c

--- rdma_usual.c
+++ rdma_usual.c
@@ -14,3 +14,21 @@ wr.sg_list = &sg_list;
 wr.dc.ah = ah[mn];
 wr.dc.dct_access_key = DCTBENCH_DCT_KEY;
 wr.dc.dct_number = remote_dct_attr[mn]->dct_num;
+
+
+if (user_param->use_event) {
+  struct ibv_cq *ev_cq;
+  void          *ev_ctx;
+  if (ibv_get_cq_event(ctx->channel, &ev_cq, &ev_ctx)) {
+    fprintf(stderr, "Failed to get cq_event\n");
+    return 1;
+  }                
+  if (ev_cq != ctx->cq) {
+    fprintf(stderr, "CQ event for unknown CQ %p\n", ev_cq);
+    return 1;
+  }
+  if (ibv_req_notify_cq(ctx->cq, 0)) {
+    fprintf(stderr, "Couldn't request CQ notification\n");
+    return 1;
+  }
+}
