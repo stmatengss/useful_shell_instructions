159600214ed0c367244d300269b66038e0ccd9b0
Teng Ma <805522925@qq.com>
2020-10-23 02:42:36 -0500

    Update network_in.sh

--- network_in.sh
+++ network_in.sh
@@ -4,3 +4,5 @@ cat /proc/version # lookup linux version
 route add default gw [GateWay] # add GateIp
 route -n # show the route table
 iperf # a C/S-based network measurement tool
+
+sar -n DEV 1 #Device TX/RX
